cdc4dc72ed2d2b9faff75515d78b349acf12ae56
Ruslan Abiev <abievruslan@yandex.ru>
2023-02-09 20:27:46 +0300

    ls_4_7_finished

---
+++ app/src/main/java/com/rusabi/firstcomposeproject/InstagramModel.kt
@@ -0,0 +1,8 @@
+package com.rusabi.firstcomposeproject
+
+data class InstagramModel(
+    val id: Int,
+    val title: String,
+    val isFollowed: Boolean
+)
+
--- app/src/main/java/com/rusabi/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/rusabi/firstcomposeproject/MainActivity.kt
@@ -6,16 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 
 import com.rusabi.firstcomposeproject.ui.theme.FirstComposeProjectTheme
@@ -41,40 +36,16 @@ fun Test(viewModel: MainViewModel) {
                 .fillMaxSize()
                 .background(color = MaterialTheme.colors.background)
         ) {
+            val models = viewModel.models.observeAsState(listOf())
             LazyColumn {
-                item {
-                    Text(text = "Title", color = Color.White)
-                }
-                items(2) {
-                    InstagramProfileCard(viewModel)
-                }
-                item {
-                    Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center
+                items(models.value) {model ->
+                    InstagramProfileCard(
+                        model = model,
+                        onFollowedButtonClickListener = {
+                            viewModel.changeFollowingStatus(it)
+                        }
                     )
-                    {
-                        Image(
-                            modifier = Modifier
-                                .size(60.dp)
-                                .clip(CircleShape)
-                                .background(color = Color.White)
-                                .padding(4.dp),
-                            painter = painterResource(id = R.drawable.ic_instagram),
-                            contentDescription = null
-                        )
-                    }
-
                 }
-                items(200) {
-                    InstagramProfileCard(viewModel)
-                }
-//                repeat(500) {
-//                    item {
-//                        InstagramProfileCard(viewModel)
-//                    }
-//                }
-
             }
         }
     }
